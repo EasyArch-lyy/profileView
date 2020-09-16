99718c7001bb2331209450505cf86ca4d5c95f4c
EasyArch-lyy <1480921056@qq.com>
2020-09-16 15:34:51 +0800

    提交

--- .reta.mock.js
+++ .reta.mock.js
@@ -13,7 +13,8 @@ import {
   getAutoTestTaskInfoList,
   getTableData,
 } from './mock';
-
+// nvocation failed Server returned invalid Response. java.lang.RuntimeException: Invocation failed Server returned invalid Response. at org.jetbrains.git4idea.http.GitAskPassXmlRpcClient.askUsername(GitAskPassXmlRpcClient.java:50) at org.jetbrains.git4idea.http.GitAskPassApp.main(GitAskPassApp.java:64) Caused by: java.io.IOException: Server returned invalid Response. at org.apache.xmlrpc.LiteXmlRpcTransport.sendRequest(LiteXmlRpcTransport.java:242) at org.apache.xmlrpc.LiteXmlRpcTransport.sendXmlRpc(LiteXmlRpcTransport.java:90) at org.apache.xmlrpc.XmlRpcClientWorker.execute(XmlRpcClientWorker.java:72) at org.apache.xmlrpc.XmlRpcClient.execute(XmlRpcClient.java:194) at org.apache.xmlrpc.XmlRpcClient.execute(XmlRpcClient.java:185) at org.apache.xmlrpc.XmlRpcClient.execute(XmlRpcClient.java:178) at org.jetbrains.git4idea.http.GitAskPassXmlRpcClient.askUsername(GitAskPassXmlRpcClient.java:47) ...
+// 1 more remote: No anonymous write access. Authentication failed for 'https://github.com/EasyArch-lyy/profileView.git/'
 const mock = {
 // 支持值为 Object 和 Array
 //   'GET /api/currentUser': {
